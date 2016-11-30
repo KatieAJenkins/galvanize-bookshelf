b452b0ddca91a823372c3fac709ae641eb290c1e
KatieAJenkins <kind2karma@gmail.com>
2016-11-29 22:00:23 -0700

    post working

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,22 @@
 'use strict';
 
 const express = require('express');
-
+const knex = require('../knex');
+const {camelizeKeys , deCamelizeKeys} = require ('humps');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+
+router.get('/books', (req, res, next) => {
+  knex('books')
+  .orderBy('id')
+  .then((books) => {
+    const booksCamel = camelizeKeys(books);
+    res.send(booksCamel);
+  })
+  .catch((err) => {
+    next(err);
+  });
+});
 
 module.exports = router;
