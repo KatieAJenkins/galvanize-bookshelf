33294b7f671afa70db6e1fa04ff978c8f1b2d978
KatieAJenkins <kind2karma@gmail.com>
2016-11-30 17:23:13 -0700

    post working

--- routes/books.js
+++ routes/books.js
@@ -2,13 +2,16 @@
 
 const express = require('express');
 const knex = require('../knex');
-const {
-    camelizeKeys,
-    deCamelizeKeys
-} = require('humps');
+const {camelizeKeys,decamelizeKeys} = require('humps');
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const boom = require('boom');
+// const bodyParser = require('bodyParser');
 
+// parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }));
+// // parse application/json
+// app.use(bodyParser.json());
 
 router.get('/books', (req, res, next) => {
     knex('books')
@@ -41,21 +44,42 @@ router.get('/books/:id', (req, res, next) => {
 
 // TO DO //
 router.post('/books', (req, res, next) => {
-    knex('books')
-      .insert({
-        title: req.body.name,
-        author: req.body.author,
-        genre: req.body.genre,
-        description: req.body.description,
-        cover_url: req.body.cover_url,
-        created_at: req.body.created_at,
-        updated_at: req.body.updated_at
-      }, '*')
+  // const {title, author, genre, description, cover_url} = req.body;
+  var body = req.body;
+  var newBook = {
+    title: body.title,
+    author: body.author,
+    genre: body.genre,
+    description: body.description,
+    cover_url: body.coverUrl
+  };
 
-      .then((books) => {
-        const booksCamel = camelizeKeys(books);
-        res.send(booksCamel[0]);
+console.log(newBook);
+
+
+
+  // if (!name || !name.trim()) {
+  //     // res.status(400).send("Name must not be blank.")
+  //     next(boom.create(400, 'Name must not be blank.'));
+  //     return;
+  //   }
+  //
+
+
+    knex('books')
+      .insert(decamelizeKeys(newBook), "*")
+      .then((data) => { //data = the object that comes back
+        const bookCamel = data[0]; //finding object in array
+        // console.log("***********");
+        // console.log(bookCamel);
+        // console.log("***********");
+
+        delete bookCamel.created_at; //delete key from Object
+        delete bookCamel.updated_at;
+
+        res.send(camelizeKeys(bookCamel));
       })
+
       .catch((err) => {
         next(err);
       });
